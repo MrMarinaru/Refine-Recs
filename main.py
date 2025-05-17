94b4e6fea30c511fa1a97e940e05691f705e1111
MrMarinaru <149961671+MrMarinaru@users.noreply.github.com>
2025-05-17 13:17:27 +0530

    Initial commit structure!

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
- 
+# main.py
+
+import torch
+import pandas as pd
+
+def main():
+    print("Project is ready.")
+    print("Torch version:", torch.__version__)
+    print("Pandas version:", pd.__version__)
+
+if __name__ == "__main__":
+    main()
